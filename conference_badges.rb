443bb6573f5e179b5744e3da62731a4af9ae624d
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-27 20:30:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,9 @@ def batch_badge_creator ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "
   batch_badge_creator.each do |batch_badge_creator|
 puts "Hello, my name is #{batch_badge_creator}."
   end
+
+def assign_rooms["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  assign_rooms.each do |assign_rooms|
+    puts "#{count}. #{assign_rooms}"
+    count +=1
+  end
