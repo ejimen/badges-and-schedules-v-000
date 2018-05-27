5558329ee34a082042396d27b1603bc1aa30e7ce
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-27 20:25:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+  gets.name
+puts "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  batch_badge_creator.each do |batch_badge_creator|
+puts "Hello, my name is #{batch_badge_creator}."
+  end
